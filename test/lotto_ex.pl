8f3c89bda8cc08eab036c78a265a3ff8d9063593
kyleyoon <kk9150@hotmail.com>
2013-06-04 15:32:27 +0900

    lotto_test

---
+++ test/lotto_ex.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: lotto_ex.pl
+#
+#        USAGE: ./lotto_ex.pl  
+#
+#  DESCRIPTION: lotto one line
+#
+#       AUTHOR:  (|Kyle Yoon|), |kk9150@hotmail.com|
+#      VERSION: 1.0
+#      CREATED: 06/04/2013 15:28:22
+#===============================================================================
+
+use 5.016;
+use warnings;
+use utf8;
+
+my $i = 0;
+printf "%d ", int(rand()*45 + 1),($i += 1)  while $i < 6;
+
+#perl -E 'printf "%d ", int(rand()*45 + 1),($i += 1)  while $i < 6;'
